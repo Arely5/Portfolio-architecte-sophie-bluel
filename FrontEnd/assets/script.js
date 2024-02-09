a0093af5f48c6c34b5d278198d3f34e3346f2015
Arely <valentinaadelaide@gmail.com>
2024-02-10 00:56:41 +0100

    JS manipulating data, figures removed from html

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,24 +2,36 @@
 async function dataFetching() {
     let data = await fetch("http://localhost:5678/api/works");
     let response = await data.json();
-    
-    console.log(response);
+    const imagesSources = [];
+    const captionContent = [];
+
+    response.forEach(element => {
+        imagesSources.push(element.imageUrl);
+        captionContent.push(element.title);
+    });
+
+    createGalleryElements(imagesSources, captionContent);
 }
 
 dataFetching();
 
 /*Creating gallery elements*/
-const gallery = document.getElementById("gallery");
-const figure = document.createElement("figure");
-const img = document.createElement("img");
-/*function createFigureElement () {
+function createGalleryElements(imagesSources, captionContent) {
+    const gallery = document.getElementById("gallery");
 
-} */
-const figcaption = document.createElement("figcaption");
+    for (let i = 0; i < imagesSources.length; i++) {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
 
-gallery.appendChild(figure);
-figure.appendChild(img);
-figure.appendChild(figcaption);
+        img.src = imagesSources[i];
+        figcaption.textContent = captionContent[i];
+        
+        gallery.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+    }
+}
 
 /*figcaption.innerText = "Test";
 img.src = "assets/images/abajour-tahina.png"; */
