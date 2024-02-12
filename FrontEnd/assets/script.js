153a31eb3a948029fa047708e67fd7aee83d9b2f
Arely <valentinaadelaide@gmail.com>
2024-02-12 22:31:15 +0100

    corrections JS gallery elements and filters

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,60 +1,51 @@
-/*Fetching data from API*/
+/*Fetching data from API and creating gallery elements*/
 async function dataFetching() {
     let data = await fetch("http://localhost:5678/api/works");
     let response = await data.json();
-    const imagesSources = [];
-    const captionContent = [];
-    const worksCategories = [];
-
-    response.forEach(element => {
-        imagesSources.push(element.imageUrl);
-        captionContent.push(element.title);
-        worksCategories.push(element.categoryId);
-    });
-
-    createGalleryElements(imagesSources, captionContent);
-    filterByCategory(worksCategories);
-}
-
-dataFetching();
-
-/*Creating gallery elements*/
-function createGalleryElements(imagesSources, captionContent) {
     const gallery = document.getElementById("gallery");
 
-    for (let i = 0; i < imagesSources.length; i++) {
+    response.forEach(element => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
 
-        img.src = imagesSources[i];
-        figcaption.textContent = captionContent[i];
+        img.src = element.imageUrl;
+        figcaption.textContent = element.title;
+        figure.setAttribute("data-id", element.categoryId);
+        figure.setAttribute("alt", element.title);
+        
         
         gallery.appendChild(figure);
         figure.appendChild(img);
         figure.appendChild(figcaption);
-    }
+    });
 }
 
+dataFetching();
+
 /* Categories filter */
-async function filterByCategory(worksCategories) {
+async function createCategoryElement() {
     let categoriesData = await fetch("http://localhost:5678/api/categories");
     let responseCategories = await categoriesData.json();
-    const categoryId = [];
-    const categoryName = [];
     const filtersWrap = document.getElementById("filters");
 
-    responseCategories.forEach(element => {
-        categoryId.push(element.id);
-        categoryName.push(element.name);
-    });
-
-
-    for (let i = 0; i < categoryId.length; i++) {
+    responseCategories.forEach(category => {
         const filtersElements = document.createElement("li");
 
         filtersElements.className = "filtersElements";
-        filtersElements.textContent = categoryName[i];
+        filtersElements.textContent = category.name;
+        filtersElements.setAttribute("data-id", category.id);
         filtersWrap.appendChild(filtersElements);
-    }
+
+        filtersElements.addEventListener("click", filterByCategory);
+        filterByCategory(filtersElements);
+    });
+}
+
+createCategoryElement();
+
+function filterByCategory(filtersElements, worksCategories ) {
+    console.log("it works");
+    /*utiliser  getAttribute*/
+    /* Logique de comparaison */
 }
