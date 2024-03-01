a46daa1b4c96b001420ff7fc673a593a0a0bdee8
Arely <valentinaadelaide@gmail.com>
2024-03-01 23:18:57 +0100

    function to allow user login or not

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,22 +2,55 @@ const loginForm = document.getElementById("login-form");
 
 loginForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    login();
+    inputRetrival();
 });
 
-function login() {
+function inputRetrival() {
     let inputEmail = document.getElementById("mail");
-    let inputPassword = document.getElementById("password");
     let email = inputEmail.value;
-    let password = inputPassword.value;
     let regExEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i);
     let isMailCorrect = regExEmail.test(email);
     let errorMessage = document.getElementById("error-message");
 
     if (isMailCorrect === true) {
         errorMessage.innerHTML = "";
-        console.log("bonne adresse mail");
     } else {
         errorMessage.innerHTML = "L'adresse e-mail n'est pas valide";
     }
+
+    login();
+}
+
+function login() {
+    let inputEmail = document.getElementById("mail");
+    let email = inputEmail.value;
+    let inputPassword = document.getElementById("password");
+    let password = inputPassword.value;
+    let postData = {
+        email: email,
+        password: password
+    };
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => {
+        if(!response.ok) {
+            throw new Error('Erreur lors de la requête: ' + response.statusText);
+        }
+
+        window.location.href = "index.html";
+
+        return response.json;
+    })
+    .catch(error => {
+        let errorMessage = document.getElementById("error-message");
+        
+        errorMessage.innerHTML = "Erreur dans l'adresse e-mail ou le mot de passe";
+        console.error('Erreur: ' + error);
+    });
 }
