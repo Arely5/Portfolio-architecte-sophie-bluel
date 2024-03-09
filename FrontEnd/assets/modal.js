2863d37b77a9f9867e82c916487dc22bcb9cc2c4
Arely <valentinaadelaide@gmail.com>
2024-03-09 22:32:48 +0100

    edition banner

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     let authToken = sessionStorage.getItem('authToken');
     let displayModify = document.getElementById("modify");
     let filters = document.querySelector(".filters");
+    let header = document.querySelector("header");
+    let editionBanner = document.getElementById("edition-banner");
     let logOut = document.getElementById("login-link");
 
     logOut.addEventListener('click', function(event) {
@@ -11,6 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     if (authToken) {
         displayModify.style.display = "flex";
         filters.style.display = "none";
+        header.style.marginTop = "95px";
+        editionBanner.style.display = "flex";
         logOut.innerText = "logout";
     }
 })
